04827c7bc1fff61aef14094b68554ca75296859e
Yasmeen_sale7 <yasmeensaleh747@gmail.com>
2025-08-20 14:12:05 +0300

    first commit

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Post;
@@ -18,19 +19,17 @@ class BlogController extends Controller
         $post = Post::findOrFail($id);
         return view('blog.show', compact('post'));
     }
-   public function like(Post $post)
-{
+    public function like(Post $post)
+    {
 
-    if ($post->likes()->where('user_id', Auth::id())->exists()) {
-        $post->likes()->where('user_id',Auth::id())->delete();
-    } else {
-        $post->likes()->create([
-            'user_id' =>Auth::id(),
-        ]);
+        if ($post->likes()->where('user_id', Auth::id())->exists()) {
+            $post->likes()->where('user_id', Auth::id())->delete();
+        } else {
+            $post->likes()->create([
+                'user_id' => Auth::id(),
+            ]);
+        }
+
+        return back();
     }
-
-    return back();
-}
-
-
 }
