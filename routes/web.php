927e96b9a4388e80d427df3c08faba838a01fa45
Yasmeen_sale7 <yasmeensaleh747@gmail.com>
2025-08-21 18:24:11 +0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,9 @@ Route::get('/', [BlogController::class, 'index'])->name('posts.index');
 Route::get('/post/{id}', [BlogController::class, 'show'])->name('post.show');
 Route::get('/posts/create', [BlogController::class, 'create'])->name('posts.create')->middleware('auth');
 Route::post('/posts', [BlogController::class, 'store'])->name('posts.store')->middleware('auth');
+Route::get('/post/{id}/edit', [BlogController::class, 'edit'])->name('posts.edit')->middleware('auth');
+Route::put('/post/{id}', [BlogController::class, 'update'])->name('posts.update')->middleware('auth');
+Route::delete('/post/{id}', [BlogController::class, 'destroy'])->name('posts.destroy')->middleware('auth');
 
 Route::post('/posts/{post}/like', [BlogController::class, 'like'])
     ->name('posts.like')
