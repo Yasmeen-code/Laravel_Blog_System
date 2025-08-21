8a0585c92bbce92719de5ed8c153c96a53de1691
Yasmeen_sale7 <yasmeensaleh747@gmail.com>
2025-08-21 18:22:25 +0300

    first commit

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -60,4 +60,38 @@ class BlogController extends Controller
 
         return redirect()->route('post.show', $post->id)->with('success', 'Post created successfully!');
     }
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+        return view('blog.edit', compact('post'));
+    }
+    public function update(Request $request, $id)
+    {
+        $post = Post::findOrFail($id);
+
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        $post->title = $request->title;
+        $post->content = $request->content;
+
+        if ($request->hasFile('image')) {
+            $imageName = time() . '.' . $request->file('image')->extension();
+            $request->file('image')->move(public_path('imgs'), $imageName);
+            $post->image = 'imgs/' . $imageName;
+        }
+
+        $post->save();
+
+        return redirect()->route('post.show', $post->id)->with('success', 'Post updated successfully!');
+    }
+    public function destroy($id)
+    {
+        $post = Post::findOrFail($id);
+        $post->delete();
+        return redirect()->route('posts.index')->with('success', 'Post deleted successfully!');
+    }
 }
